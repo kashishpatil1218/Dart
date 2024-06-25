235bba4620ccb55a57e3845425e82d0270bfd4cd
Kashish patil <156797313+kashishpatil1218@users.noreply.github.com>
2024-06-25 14:51:42 +0530

    Update flutter.dart

--- flutter.dart
+++ flutter.dart
@@ -1,5 +1,5 @@
 import 'dart:io';
-class employee
+class Employee
 {
     int? id,salary,age;
     String? name,designation;
@@ -8,35 +8,35 @@ class employee
 
 void main()
 {
-    List<employee> Employee=[];
+    List<Employee> employee=[];
    for(int i=0;i<5;i++)
 {
-    employee s1=employee();
-    Employee.add(s1);
+    Employee s1=Employee();
+    employee.add(s1);
 }
 for(int i=0;i<5;i++)
 {
     stdout.write("enter the ID : ");
-    Employee[i].id=int.parse(stdin.readLineSync()!);
+    employee[i].id=int.parse(stdin.readLineSync()!);
 
     stdout.write("enter the salary : ");
-    Employee[i].salary=int.parse(stdin.readLineSync()!);
+    employee[i].salary=int.parse(stdin.readLineSync()!);
 
     stdout.write("enter the age : ");
-    Employee[i].age=int.parse(stdin.readLineSync()!);
+    employee[i].age=int.parse(stdin.readLineSync()!);
 
     stdout.write("enter the name : ");
-    Employee[i].name=stdin.readLineSync()!;
+    employee[i].name=stdin.readLineSync()!;
 
     stdout.write("enter the designation : ");
-    Employee[i].designation= stdin.readLineSync()!;
+    employee[i].designation= stdin.readLineSync()!;
 
 
 
 }
 for(int i=0;i<5;i++)
 {
-    print("${Employee[i].id} ${Employee[i].salary} ${Employee[i].age} ${Employee[i].name} ${Employee[i].designation}");
+    print("${employee[i].id} ${employee[i].salary} ${employee[i].age} ${employee[i].name} ${employee[i].designation}");
 }
 }
 
