9edd491e617a1f3ac7474ad20b9f9c1529f255fe
Kashish patil <156797313+kashishpatil1218@users.noreply.github.com>
2024-06-25 14:53:14 +0530

    Update flutter.dart

--- flutter.dart
+++ flutter.dart
@@ -19,17 +19,22 @@ for(int i=0;i<5;i++)
     stdout.write("enter the ID : ");
     employee[i].id=int.parse(stdin.readLineSync()!);
 
-    stdout.write("enter the salary : ");
-    employee[i].salary=int.parse(stdin.readLineSync()!);
+    stdout.write("enter the name : ");
+    employee[i].name=stdin.readLineSync()!;
 
     stdout.write("enter the age : ");
     employee[i].age=int.parse(stdin.readLineSync()!);
 
-    stdout.write("enter the name : ");
-    employee[i].name=stdin.readLineSync()!;
+    stdout.write("enter the salary : ");
+    employee[i].salary=int.parse(stdin.readLineSync()!);
 
     stdout.write("enter the designation : ");
     employee[i].designation= stdin.readLineSync()!;
+
+    
+
+   
+    
 
 
 
